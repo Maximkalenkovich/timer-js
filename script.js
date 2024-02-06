759c7d68d35e98ae6648ff9c1f5cbe2a4e0d9e25
Maximkalenkovich <peterson.maxim@mail.ru>
2024-02-06 22:00:09 +0300

    add new function for secunda and minute and hours . my timer complite

--- script.js
+++ script.js
@@ -6,7 +6,6 @@ const stopButton = document.getElementById('stop')
 
 let timer = 0
 
-debugger
 startButton.addEventListener('click',function () {
     timer += 1
      return tableElement.textContent = timer.toString()
@@ -17,24 +16,56 @@ stopButton.addEventListener('click', function (){
 })
 
 
+
+
 //timer
 
-const timerTableElement = document.getElementById("timerTable")
+const secundaTable = document.getElementById("secunda")
+const minuteTable = document.getElementById('minute');
+const hoursTable = document.getElementById('hours');
 
 const startTimerButton = document.getElementById('startTimer')
-
-const stopTimerButton = document.getElementById('stopTimer')
-
-
-let table = 0
+const pauseTimerButton = document.getElementById('pauseTimer')
 
 
-    startTimerButton.addEventListener('click', function(){
-        debugger
-        setInterval(function (){
-            table += 1
-          return   timerTableElement.textContent = table.toString()
-        },1000)
-    })
 
 
+let secunda = 0;
+let start;
+startTimerButton.addEventListener('click', function() {
+  start = setInterval(function() {
+       secunda++;
+       secundaTable.textContent = secunda.toString();
+        minuteHandler(secunda);
+        if (secunda >= 60){
+            return secunda = 0
+        }
+
+    }, 1000);
+});
+
+
+pauseTimerButton.addEventListener('click',function (){
+    clearTimeout(start)
+})
+
+let minute = 0;
+function minuteHandler(sec) {
+    if (sec === 60) {
+        minute++;
+        minuteTable.textContent = minute.toString();
+        hoursHandler(minute)
+        minute >= 60 ? minute = 0: minute
+    }
+}
+
+let hours = 0;
+function hoursHandler(min) {
+    if (min === 60){
+        hours++;
+        hoursTable.textContent=hours.toString()
+        if (hours >= 24){
+            return(hours=0)
+        }
+    }
+}
